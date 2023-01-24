752095b5461e6a6d80d8542fca15d2f2067ac532
Ben <ncor89b@gmail.com>
2023-01-24 10:46:04 +0100

    adding Logo component and strat creating articles design

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { groq } from "next-sanity";
+import Image from "next/image";
 import { client } from "../../../../lib/sanity.client";
+import urlFor from "../../../../lib/urlFor";
 
 type Props = {
   params: {
@@ -18,12 +20,44 @@ async function Post({params: { slug }}: Props) {
   `;
 
   const post: Post = await client.fetch(query, { slug });
-  
-  console.log(post);
-  
 
+  
+  // TODO RESTART HERE LIGNE 40 👇
+
+  
   return (
-    <div>Post: {slug}</div>
+    <article className="px-10 pb-28">
+      <section className="space-x-2 border border-[#C5D2FC] text-white">
+        <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
+          <div className="absolute top-0 w-full h-full opacity-10 blur-sm p-10">
+          <Image
+            className="object-cover object-center mx-auto"
+            src={urlFor(post.mainImage).url()}
+            alt={post.author.name}
+            fill
+          />
+          </div>
+
+
+          <section className="p-5 bg-[#C5D2FC] w-full">
+            <div className="">
+              <div className="">
+                <h1 className="text-[#211F35] text-4xl font-extrabold">
+                  {post.title}
+                </h1>
+                <p className="text-[#211F35] sm:text-base text-xs">
+                  {new Date(post._createdAt).toLocaleDateString("en-US", {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric",
+                  })}
+                </p>
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
+    </article>
   )
 }
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
+import LogoTwitter from "./LogoTwitter"
 
 function Header() {
   return (
@@ -19,16 +20,17 @@ function Header() {
 
 
       <div className="">
-      <Link 
+        <Link 
             href="https://twitter.com/Bendevweb89/"
             className="px-8 py-2 text-sm md:text-base bg-[#211F35] text-white flex items-center rounded-full text-center"
-          >
-            BenDevweb
-          </Link>
+        >
+          BenDevweb
+          <LogoTwitter className="text-[#C5D2FC] ml-3 mt-0.5" />
+        </Link>
       </div>
 
     </header>
   )
 }
 
-export default Header
+export default Header;
